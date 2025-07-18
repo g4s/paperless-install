bcd46326536cf03d41b905d19b871abb68e88706
Gregor Segner <gregor.segner@gmail.com>
2025-07-18 14:28:42 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -8,7 +8,8 @@ if [[ $(command -v podman) ]];  then
         --name=paperless \
         --publish 8000:8000 \
         --publish 8001:3000 \
-        --publish 8002:8080 
+        --publish 8002:8080 \
+        --label=tsdproxy.enable=true
 
     podman volume create paperless-redis
     podman volume create paperless-database
